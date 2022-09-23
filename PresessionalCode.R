0434c668ec1434afb38ee6be0cb691711ff2e91b
ettelocin <nikkisjunkmail@gmail.com>
2022-09-23 10:30:48 +0100

    update

--- PresessionalCode.R
+++ PresessionalCode.R
@@ -1,5 +1,27 @@
+# Day 1
 # September 22
+#
+
+rm(list = ls())
+library("tidyverse")
 
 todayTemp <- 30
 
 temps <- c(34, 24)
+
+temps <- 20:30
+
+profs <- c("Sullivan","Li","Kappes")
+
+theSkyIsBlue <- TRUE
+
+temp <- c(23, 45, 12, 23, 23, 43, 42, 34)
+
+aveTemp <- mean(temp)
+
+# how to get the first and seventh position 
+temp[c(1,7)]
+
+
+# Day 2
+# September 23
